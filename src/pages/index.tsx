0fed870426fe752e267cfeb7e612ccd8431499fa
yumao <2310087518@qq.com>
2023-06-25 22:24:39 +0800

    feat：准备开始组件封装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
 import styles from './index.less';
-
+import axios  from 'axios';
 export default function IndexPage() {
+  const getData = () => {
+    const options = { 标题：title ， 导演 ：director，主演：actor，地区：region，上映：releaseTime，分类：videoType} 
+    axios.get(`https://api.pingcc.cn/video/search/${JSON.stringify('title="123"')}/{完美世界}`).then(res => console.log(res))
+  }
+
+  useEffect(()=> {
+    getData()
+  },[])
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
