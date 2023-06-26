62e3f2742a96191baed424542f7368165ce60698
hongguoqiang <2310087518@qq.com>
2023-06-26 14:00:00 +0800

    feat:配置依赖;

--- .umirc.ts
+++ .umirc.ts
@@ -4,8 +4,7 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
+  // plugins: ['@umijs/plugins/dist/styled-components'],
   fastRefresh: {},
 });
