49e85d97442354df374636378ff6daa24c9ecd8e
hongguoqiang <2310087518@qq.com>
2023-06-26 13:59:21 +0800

    feat:配置依赖

---
+++ src/pages/Components/index.tsx
@@ -0,0 +1,7 @@
+import { useRef } from 'react';
+
+export default function App() {
+  const audio = useRef<HTMLAudioElement>(null);
+  return <audio ref={audio}></audio>;
+}
+// https://github.com/lgf196/JoL-player.git
--- src/pages/conponent/index.tsx
+++
@@ -1,7 +0,0 @@
-import {useRef} from 'react'
-
-export default function App () {
-    const audio = useRef<HTMLAudioElement>(null)
-    return <audio ref={audio}></audio>
-}
-//https://github.com/lgf196/JoL-player.git
